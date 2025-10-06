a75a1dec7ba6b5b3f08fa9c0c1e3a8c4cfa7e73b
Navjot Kaur <navjotkaur1440@gmail.com>
2025-10-06 15:37:01 +0530

    adding js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-gsap.to(" ")
+document.addEventListener()
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    duration: 0.5,
+    height: "80px",
+    // delay:1
+    scrollTrigger:{
+        trigger: "#nav",
+        scroller:"body", 
+        markers: true,
+        start:"top -10%",
+        end: "top -11%",
+        scrub:3
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub:2 
+    }
+})
